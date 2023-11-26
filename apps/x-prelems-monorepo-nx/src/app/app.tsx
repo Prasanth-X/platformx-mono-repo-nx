4bea7225aeee52b3a27bb3936cc30d03e7e0c854
Prasanth <prashanthmsktm@gmail.com>
2023-11-26 12:09:08 +0100

    Initial commit

---
+++ apps/x-prelems-monorepo-nx/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="x-prelems-monorepo-nx" />
+    </StyledApp>
+  );
+}
+
+export default App;
