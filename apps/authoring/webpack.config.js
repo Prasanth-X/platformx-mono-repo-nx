d096e5da958047ad8ad4db82685f3eaa273f03ff
Prasanth <prashanthmsktm@gmail.com>
2023-12-15 07:01:34 +0100

    Webpack added

--- apps/authoring/webpack.config.js
+++ apps/authoring/webpack.config.js
@@ -1,7 +1,7 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 
-// Nx plugins for webpack.
+
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   return config;
 });
