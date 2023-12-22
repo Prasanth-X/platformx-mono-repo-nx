aaed4a76b8ea18ee6da8470a30d99ad5fdcc9992
Prasanth <prashanthmsktm@gmail.com>
2023-12-22 10:43:05 +0100

    MAER-2738:content code added

--- libs/utilities/src/lib/components/XLoader/XLoader.stories.tsx
+++ libs/utilities/src/lib/components/XLoader/XLoader.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { XLoader } from './XLoader';
+import XLoader  from './XLoader';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
